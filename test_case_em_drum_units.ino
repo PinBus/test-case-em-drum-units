ae4253cd5a6e17c5b4851100ab1ec1f82acc5ca0
Thomas Gravekamp <me@thomas-gravekamp.nl>
2023-08-30 10:36:27 +0200

    WIP

--- test_case_em_drum_units.ino
+++ test_case_em_drum_units.ino
@@ -2,6 +2,7 @@
  * Test-Case - EM Drum Units
  *
  * Version 1.0 (01-12-2021)
+ * Version 1.1 (24-08-2023)
  **/
 
 #include <Wire.h>
@@ -59,35 +60,6 @@ struct SwitchState {
     SwitchCallback on_switch_open;
 };
 
-enum LedAnimationState {
-    Running = 0,
-    Pauzed = 1,
-    FadeIn = 2,
-    FadeOut = 3
-};
-
-enum LedAnimationType {
-    Static = 0,
-    Fade = 1
-};
-
-struct LedAnimationStep {
-    LedAnimationType type;
-    uint32_t duration;
-    uint16_t pwm_value;
-};
-
-struct LedAnimation {
-    LedAnimationStep* steps;
-    uint32_t last_update;
-    uint8_t current_step;
-    uint16_t current_pwm_value;
-    uint16_t backup_pwm_value;
-    uint8_t number_of_steps;
-    uint8_t pin;
-    LedAnimationState state;
-};
-
 struct DrumUnit {
     uint8_t pin;
     uint32_t timer;
@@ -105,11 +77,14 @@ bool state_should_reset = true;
 // To keep track whether we're resetting for the first time or not
 bool state_is_initial_reset = true;
 
-// Starts out on false, as it has to wait for the inital reset and it is false during regular resets
+// Starts out on false, as it has to wait for the initial reset and it is false during regular resets
 bool state_is_drum_unit_allowed = false;
 
 // Is false when the reset button can not be used
 bool state_is_reset_allowed = false;
+
+// Timer to keep track of the cooldown on buttons
+uint32_t state_last_button_press = 0;
 
 // Track whether another drum unit needs to be triggered to simulate a carry
 bool state_has_carry_for_10 = false;
@@ -130,6 +105,8 @@ bool state_has_carry_for_1000 = false;
 #define LED_QUARTER_VALUE 1024
 
 #define INITIAL_RESET_AFTER_MS 250
+
+#define BUTTON_COOLDOWN_MS 2000
 
 /** Heartbeat LED **/
 uint8_t heartbeat_led_index = 0;
@@ -403,229 +380,6 @@ void reset_drum_units() {
     state_is_drum_unit_allowed = false;
 }
 
-/** LED animations **/
-
-// Thousands button animation
-LedAnimationStep led_1000_animation_sequence[6] = {
-    { LedAnimationType::Static, 125, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 100, LED_MAX_VALUE },
-    { LedAnimationType::Static, 2775, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 500, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 100, LED_MAX_VALUE },
-    { LedAnimationType::Static, 2400, LED_QUARTER_VALUE }
-};
-
-LedAnimation led_1000_animation = {
-    led_1000_animation_sequence,
-    1,
-    0,
-    0,
-    LED_HALF_VALUE,
-    6,
-    PIN_PCA9685_LED_BUTTON_1000,
-    LedAnimationState::Running
-};
-
-// Hundreds button animation
-LedAnimationStep led_100_animation_sequence[6] = {
-    { LedAnimationType::Static, 250, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 100, LED_MAX_VALUE },
-    { LedAnimationType::Static, 2650, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 375, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 100, LED_MAX_VALUE },
-    { LedAnimationType::Static, 2525, LED_QUARTER_VALUE }
-};
-
-LedAnimation led_100_animation = {
-    led_100_animation_sequence,
-    1,
-    0,
-    0,
-    LED_HALF_VALUE,
-    6,
-    PIN_PCA9685_LED_BUTTON_100,
-    LedAnimationState::Running
-};
-
-// Tens button animation
-LedAnimationStep led_10_animation_sequence[6] = {
-    { LedAnimationType::Static, 375, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 100, LED_MAX_VALUE },
-    { LedAnimationType::Static, 2525, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 250, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 100, LED_MAX_VALUE },
-    { LedAnimationType::Static, 2650, LED_QUARTER_VALUE }
-};
-
-LedAnimation led_10_animation = {
-    led_10_animation_sequence,
-    1,
-    0,
-    0,
-    LED_HALF_VALUE,
-    6,
-    PIN_PCA9685_LED_BUTTON_10,
-    LedAnimationState::Running
-};
-
-// Ones button animation
-LedAnimationStep led_1_animation_sequence[6] = {
-    { LedAnimationType::Static, 500, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 100, LED_MAX_VALUE },
-    { LedAnimationType::Static, 2400, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 125, LED_QUARTER_VALUE },
-    { LedAnimationType::Static, 100, LED_MAX_VALUE },
-    { LedAnimationType::Static, 2775, LED_QUARTER_VALUE }
-};
-
-LedAnimation led_1_animation = {
-    led_1_animation_sequence,
-    1,
-    0,
-    0,
-    LED_HALF_VALUE,
-    6,
-    PIN_PCA9685_LED_BUTTON_1,
-    LedAnimationState::Running
-};
-
-// Reset button animation
-LedAnimationStep led_reset_animation_sequence[1] = {
-    { LedAnimationType::Static, 1000, LED_HALF_VALUE }
-};
-
-LedAnimation led_reset_animation = {
-    led_reset_animation_sequence,
-    1,
-    0,
-    0,
-    LED_HALF_VALUE,
-    1,
-    PIN_PCA9685_LED_BUTTON_RESET,
-    LedAnimationState::Pauzed
-};
-
-#define LED_ANIMATION_NUMBER_OF 5
-LedAnimation animations[LED_ANIMATION_NUMBER_OF] = {
-    led_1000_animation,
-    led_100_animation,
-    led_10_animation,
-    led_1_animation,
-    led_reset_animation
-};
-
-// Scratchpad pointers for loops iterating over animations and animation steps
-LedAnimation* animation = NULL;
-LedAnimationStep* step = NULL;
-
-// Warning: Hairy code ahead
-void update_leds() {
-    for (uint8_t i = 0; i < LED_ANIMATION_NUMBER_OF; i++) {
-        animation = &animations[i];
-
-        if (animation->state == LedAnimationState::Pauzed) {
-            // Animation is pauzed, don't do anything
-            continue;
-        }
-
-        if (animation->state == LedAnimationState::FadeOut) {
-            // Alternate flow, fading out is not part of the normal animation flow
-            if (millis() - animation->last_update > LED_FADE_SPEED_MS) {
-                // The fade out is done, set the current pwm value to 0
-                animation->current_pwm_value = 0;
-                pca9685.setPin(animation->pin, animation->current_pwm_value, true);
-                // Update animation state
-                animation->state = LedAnimationState::Pauzed;
-                continue;
-            }
-
-            // Compute intermediary value of the fade out
-            uint32_t time_in = millis() - animation->last_update;
-            animation->current_pwm_value = map(time_in, 0, LED_FADE_SPEED_MS, animation->backup_pwm_value, 0);
-            pca9685.setPin(animation->pin, animation->current_pwm_value, true);
-            continue;
-        }
-
-        if (animation->state == LedAnimationState::FadeIn) {
-            // Alternate flow, fading in is not part of the normal animation flow
-            step = &animation->steps[0];
-
-            if (millis() - animation->last_update > LED_FADE_SPEED_MS) {
-                // The fade in is done, set the current pwm value to the first step's pwm value
-                animation->current_pwm_value = step->pwm_value;
-                pca9685.setPin(animation->pin, animation->current_pwm_value, true);
-                // Update animation state. Let the animation resetart from the first step
-                animation->state = LedAnimationState::Running;
-                animation->last_update = millis();
-                animation->current_step = 0;
-                continue;
-            }
-
-            // Compute intermediary value of the fade in
-            uint32_t time_in = millis() - animation->last_update;
-            animation->current_pwm_value = map(time_in, 0, LED_FADE_SPEED_MS, 0, step->pwm_value);
-            pca9685.setPin(animation->pin, animation->current_pwm_value, true);
-            continue;
-        }
-
-        step = &animation->steps[animation->current_step];
-
-        if (step->type == LedAnimationType::Fade) {
-            // Compute intermediary value of the fade step
-            uint16_t previous_pwm_value = animation->steps[animation->current_step == 0 ? animation->number_of_steps - 1 : animation->current_step - 1].pwm_value;
-            uint32_t time_in = millis() - animation->last_update;
-            animation->current_pwm_value = map(time_in, 0, step->duration, previous_pwm_value, step->pwm_value);
-            pca9685.setPin(animation->pin, animation->current_pwm_value, true);
-        }
-
-        if (millis() - animation->last_update > step->duration) {
-            // Move on to the next animation step if the current step is done
-            animation->last_update = millis();
-            animation->current_step += 1;
-            if (animation->current_step == animation->number_of_steps) {
-                animation->current_step = 0;
-            }
-
-            step = &animation->steps[animation->current_step];
-            if (step->type == LedAnimationType::Static) {
-                // If the step is a static pwm value, set it
-                animation->current_pwm_value = step->pwm_value;
-                pca9685.setPin(animation->pin, step->pwm_value, true);
-            }
-        }
-    }
-}
-
-void stop_animation(uint8_t animation_index) {
-    animation = &animations[animation_index];
-
-    if (animation->state == LedAnimationState::Pauzed) {
-        // Don't do anything if the animation is already pauzed
-        return;
-    }
-
-    // Update animation state
-    animation->state = LedAnimationState::FadeOut;
-    animation->last_update = millis();
-    // Set the backup pwm field. This value is needed when computing the new current
-    // value while fading out. The pwm value of the current step can not be used as
-    // that step might not yet be completed.
-    animation->backup_pwm_value = animation->current_pwm_value;
-}
-
-void resume_animation(uint8_t animation_index) {
-    animation = &animations[animation_index];
-
-    if (animation->state == LedAnimationState::Running) {
-        // Don't do anything if the animation is already running
-        return;
-    }
-
-    // Update animation state
-    animation->state = LedAnimationState::FadeIn;
-    animation->last_update = millis();
-}
-
 /** Serial **/
 void init_serial() {
     // Open the serial connection
@@ -642,12 +396,78 @@ void update_serial() {
     }
 }
 
+/** State **/
+void start_button_cooldown() {
+    if (state_last_button_press != 0) {
+        return;
+    }
+
+    state_last_button_press = millis();
+
+    visualize_buttons();
+}
+
+void update_button_cooldown() {
+    if (state_last_button_press == 0) {
+        // No active cooldown
+        return;
+    }
+
+    if (drum_unit_reset_timer != 0) {
+        // Do not enable the buttons as long as the reset sequence is running.
+        return;
+    }
+
+    if (millis() - state_last_button_press > BUTTON_COOLDOWN_MS) {
+        state_last_button_press = 0;
+
+        visualize_buttons();
+    }
+}
+
+bool is_drum_button_allowed() {
+    return state_last_button_press == 0 && state_is_drum_unit_allowed;
+}
+
+bool is_reset_button_allowed() {
+    return state_last_button_press == 0 && state_is_reset_allowed;
+}
+
+void visualize_buttons() {
+    uint16_t reset_button_value = is_reset_button_allowed() ? LED_MAX_VALUE : LED_QUARTER_VALUE;
+    uint16_t drum_button_value = is_drum_button_allowed() ? LED_MAX_VALUE : LED_QUARTER_VALUE;
+
+    pca9685.setPin(PIN_PCA9685_LED_BUTTON_RESET, reset_button_value, true);
+    pca9685.setPin(PIN_PCA9685_DRUM_UNIT_1, drum_button_value, true);
+    pca9685.setPin(PIN_PCA9685_DRUM_UNIT_10, drum_button_value, true);
+    pca9685.setPin(PIN_PCA9685_DRUM_UNIT_100, drum_button_value, true);
+    pca9685.setPin(PIN_PCA9685_DRUM_UNIT_1000, drum_button_value, true);
+}
+
+void update_state() {
+    // If all drum units are at position 0, do not allow resetting
+    // Only update the animation when the new state differs from the old state
+    if ((drum_units[0].position == 0 &&
+        drum_units[1].position == 0 &&
+        drum_units[2].position == 0 &&
+        drum_units[3].position == 0) != state_is_reset_allowed) {
+
+        // Update state
+        state_is_reset_allowed = !state_is_reset_allowed;
+
+        // Update button LEDs
+        visualize_buttons();
+    }
+}
+
 /** Button actions **/
 void on_1000_button_down() {
-    if (!state_is_drum_unit_allowed || drum_units[0].timer) {
+    if (!is_drum_button_allowed() || drum_units[0].timer) {
         // Prevent firing drum unit while resetting or while firing
         return;
     }
+
+    start_button_cooldown();
 
     fire_drum_unit(0);
 
@@ -655,10 +475,12 @@ void on_1000_button_down() {
 }
 
 void on_100_button_down() {
-    if (!state_is_drum_unit_allowed || drum_units[1].timer) {
+    if (!is_drum_button_allowed() || drum_units[1].timer) {
         // Prevent firing drum unit while resetting or while firing
         return;
     }
+
+    start_button_cooldown();
 
     fire_drum_unit(1);
 
@@ -671,10 +493,12 @@ void on_100_button_down() {
 }
 
 void on_10_button_down() {
-    if (!state_is_drum_unit_allowed || drum_units[2].timer) {
+    if (!is_drum_button_allowed() || drum_units[2].timer) {
         // Prevent firing drum unit while resetting or while firing
         return;
     }
+
+    start_button_cooldown();
 
     fire_drum_unit(2);
 
@@ -692,10 +516,12 @@ void on_10_button_down() {
 }
 
 void on_1_button_down() {
-    if (!state_is_drum_unit_allowed || drum_units[3].timer) {
+    if (!is_drum_button_allowed() || drum_units[3].timer) {
         // Prevent firing drum unit while resetting or while firing
         return;
     }
+
+    start_button_cooldown();
 
     fire_drum_unit(3);
 
@@ -718,6 +544,12 @@ void on_1_button_down() {
 }
 
 void on_reset_button_down() {
+    if (!is_reset_button_allowed()) {
+        return;
+    }
+
+    start_button_cooldown();
+
     reset_drum_units();
 
     update_state();
@@ -747,45 +579,6 @@ void on_1_carry_opened() {
     state_has_carry_for_10 = false;
 }
 
-void update_state() {
-    // If all drum units are at position 0, do not allow resetting
-    // Only update the animation when the new state differs from the old state
-    if ((drum_units[0].position == 0 &&
-        drum_units[1].position == 0 &&
-        drum_units[2].position == 0 &&
-        drum_units[3].position == 0) != state_is_reset_allowed) {
-
-        // Update state
-        state_is_reset_allowed = !state_is_reset_allowed;
-
-        // Update reset button LED animation
-        if (state_is_reset_allowed) {
-            stop_animation(4);
-        }
-        else {
-            resume_animation(4);
-        }
-    }
-
-    // Drum unit buttons can not be pressed while resetting, update their LED animations accordingly
-    if (!state_is_drum_unit_allowed && !(
-        drum_units[0].position == 0 &&
-        drum_units[1].position == 0 &&
-        drum_units[2].position == 0 &&
-        drum_units[3].position == 0)) {
-        stop_animation(0);
-        stop_animation(1);
-        stop_animation(2);
-        stop_animation(3);
-    }
-    else {
-        resume_animation(0);
-        resume_animation(1);
-        resume_animation(2);
-        resume_animation(3);
-    }
-}
-
 void setup() {
     // Initialize heartbeat LED
     init_heartbeat_led();
@@ -798,6 +591,9 @@ void setup() {
 
     // Initialize serial
     init_serial();
+
+    // Visualize all buttons in a disabled state, until the initial reset has happened
+    visualize_buttons();
 }
 
 void loop() {
@@ -810,11 +606,11 @@ void loop() {
     // Update drum units
     update_drum_units();
 
-    // Update LEDs
-    update_leds();
-
     // Update serial
     update_serial();
+
+    // Update the button cooldown
+    update_button_cooldown();
 
     // Reset the drum units 250ms after startup to move them to a known position
     if (state_should_reset && millis() > INITIAL_RESET_AFTER_MS) {
